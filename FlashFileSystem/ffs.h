f69b72ab062124ea97f89c307b45d162106810cb
Gunjan Sethi <gunjan.sethi@microsemi.com>
2018-08-10 22:46:35 +0530

    FFS implementation

--- FlashFileSystem/ffs.h
+++ FlashFileSystem/ffs.h
@@ -5,60 +5,225 @@
 
 #include <stdint.h>
 #include <vector>
+#include "ffs_api.h"
 
 using namespace std;
-enum ERROR {
-	NO_ERROR = 0,
-	ERR_FFS_FILE_NOT_FOUNT = 101,
-	ERR_FFS_FILE_INTIGRITY_CHECK_FAIL,
-	ERR_FFS_PAGE_ERASE_FAIL
-};
+
+/* gs: changed error name from ERROR to  "FfsErrorCode" 
+    currently we are exposing ffs.h to user/test application, we should not exposed ffs.h 
+    Below enum should be mentioned in separate header file
+*/
+
+#define MAX_FILE_COUNT          0x03
+#define MAX_PAGE_COUNT          0x03
+
+/*! Flash Block Format */
+/*
+
++--------+--------+--------+--------+
+|          Block Read Byte          |
++--------+--------+--------+--------+
+|              Header               |
++--------+--------+--------+--------+
+|           File A Data             |
++--------+--------+--------+--------+
+|              Header               |
++--------+--------+--------+--------+
+|           File B Data             |
++--------+--------+--------+--------+
+|              Header               |
++--------+--------+--------+--------+
+|           File C Data             |
++--------+--------+--------+--------+
+|                                   |
++--------+--------+--------+--------+
+|                                   |
++--------+--------+--------+--------+
+|                                   |
++--------+--------+--------+--------+
+|                                   |
++--------+--------+--------+--------+
+*/
+
+/*! File Header Format */
+/*
+
+7                               1   0
++--------+--------+--------+--------+
+|            Magic Number           |
++--------+--------+--------+--------+
+|             File Id           |val|
++--------+--------+--------+--------+
+|           Sequence Number         |
++--------+--------+--------+--------+
+|           Sequence Number         |
++--------+--------+--------+--------+
+|             Data Length           |
++--------+--------+--------+--------+
+|             Data Length           |
++--------+--------+--------+--------+
+|             Data CRC              |
++--------+--------+--------+--------+
+|             header CRC            |
++--------+--------+--------+--------+
+|               Data                |
++--------+--------+--------+--------+
+
+*/
+
+/* LSB store first
+   So while mapping to header structure we can directly read sequence and data length */
+#define FILE_HEADER_ELE \
+    _H(MAGIC_NUM,       "Magic Number") \
+    _H(FILE_ID,         "File Identifier")  \
+    _H(SEQ_NUM_LSB,     "Sequence Number")  \
+    _H(SEQ_NUM_MSB,     "Sequence Number")  \
+    _H(DATA_LEN_LSB,    "Data Length")  \
+    _H(DATA_LEN_MSB,    "Data Length")  \
+    _H(CRC_D,           "Data CRC") \
+    _H(CRC_H,           "Header CRC")   \
+
+#undef _H
+#define _H(field, name) field##_POS,
+typedef enum
+{
+    FILE_HEADER_ELE
+    //HEADER_SIZE
+    /* DATA_START_POS */
+}headerElePos_t;
+
+
+/* This is Flashc.cpp error code, Used to resolve build error */
+typedef enum {
+    FLASH_SUCCESS,      //!< FLASH_SUCCESS
+    FLASH_OP_ERROR,     //!< FLASH_OP_ERROR
+    FLASH_WRONG_ADDRESS //!< FLASH_WRONG_ADDRESS
+} FlashErrorCode;
+
+/* Error code used internally, FFS user does not need to know this */
+typedef enum {
+    SUCCESS,
+    ERR_NO_SPACE_TO_WRITE,
+} ErrorCode;
 
 typedef struct  {
-	uint8_t pageID;
-	uint16_t * baseAddress;
-	uint16_t availableFreeMemory;
+    uint8_t pageNum;                 /*!< Page ID */
+    uint8_t * baseAddress;          /*!< Page Start Address */
+    uint16_t availableFreeMemory;   /*!< Available Free Memory in Page */
 }FFS_PAGE;
 
 typedef struct  {
-	uint8_t magic;
-	uint8_t fileID : 7;
-	uint8_t ValidBit : 1;
-	uint16_t sequenceNumber;
-	uint16_t dataLen;
-	uint8_t dataCrc;
-	uint8_t headerCrc;
+    uint8_t magic;                  /*!< Magic Number */
+    uint8_t fileID : 7;             /*!< Field ID (1-127) */
+    uint8_t ValidBit : 1;           /*!< File Info Validation Bit */
+    uint16_t sequenceNumber;        /*!< File Sequence Number */
+    uint16_t dataLen;               /*!< Data Length */
+    uint8_t dataCrc;                /*!< Data CRC */
+    uint8_t headerCrc;              /*!< Header CRC */
 }FFS_FILE_HEADER;
 
 typedef struct  {
-	FFS_FILE_HEADER header;
-	uint16_t * pDataAddress;
-	uint16_t * pfileAddress;
+    FFS_FILE_HEADER header;
+    uint8_t * pDataAddress;
+    uint8_t * pfileAddress;
 }FFS_FILE;
 
 class FFS {
 public:
-	uint8_t _initFFS(const uint16_t * AddressList, const uint8_t pageCount, const uint8_t fileCount);
-	uint8_t _getFileData(const uint8_t fileID, uint16_t ** pData, uint16_t *pDataLen);
-	uint8_t _writeFileData(const uint8_t fileID, const uint8_t * pData, const uint16_t dataLen);
-	uint16_t *_makeFile(uint8_t fileID, uint8_t * pData, uint16_t dataLen);
+
+    /*************************************************************************************************/
+    /*!
+    *  \brief      Inititalise Flash File System
+    *
+    *  \param      
+    *
+    *  \return     
+    *
+    *  Initialize the Flash Space used for storing/retrieving file information. 
+    *  This function should only be called once upon system initialization. 
+    *  This function fetches predefined static list of Page Address, number of pages and number of files
+    */
+    /*************************************************************************************************/
+    uint8_t _initFFS(const uint8_t **AddressList, const uint8_t pageCount, const uint8_t fileCount);
+
+    /*************************************************************************************************/
+    /*!
+    *  \brief     Get File Data from flash file system
+    *
+    *  \param       fileID      File ID
+    *
+    *  \param       pData       Hold address of data in flash
+    *
+    *  \param       pDataLen    Hold length of data available in flash
+    *
+    *  \return      return error code    
+    *
+    *  Get the file information of give fileID from Flash if exists, on successful read return 0 otherwise return error code.
+    */
+    /*************************************************************************************************/
+
+    uint8_t _getFileData(const uint8_t fileID, uint8_t ** pData, uint16_t *pDataLen);
+    
+    /*************************************************************************************************/
+    /*!
+    *  \brief     Write File Data to Flash File System
+    *
+    *  \param       fileID      File ID
+    *
+    *  \param       pData       Hold address of data in flash
+    *
+    *  \param       pDataLen    Hold length of data available in flash
+    *
+    *  \return      return error code
+    *
+    *  Write the file data of given fileID on Flash, on successful write return 0 otherwise return error code.
+    */
+    /*************************************************************************************************/
+    
+    uint8_t _writeFileData(const uint8_t fileID, const uint8_t * pData, const uint16_t dataLen);
+    
+    uint16_t *_makeFile(uint8_t fileID, uint8_t * pData, uint16_t dataLen);
 
 private:
-	uint8_t numberOfFiles, numberOfPages;
-	vector<FFS_FILE> files;
-	vector<FFS_PAGE> pages;
-	uint8_t currentPage;
+    uint8_t numberOfFiles = 0;
+    uint8_t numberOfPages = 0;
 
-	bool isFileExist(uint8_t fileID);
-	uint8_t cacheAllFiles(void);
-	FFS_FILE_HEADER getHeader(uint8_t fileID, uint8_t * pData, uint16_t dataLen);
-	uint8_t crc8(uint8_t * pData, uint16_t dataLen);
-	uint8_t writeToFlash(const uint16_t * pTargetAddr, const uint16_t * pSourceAddr, const uint16_t dataLen);
-	uint8_t writeFileToFlash(FFS_PAGE page, FFS_FILE file);
-	uint8_t checkFileIntigrity(FFS_FILE file);
-	uint8_t erasePage(FFS_PAGE page);
+    /* Holds all valid file information */
+    vector<FFS_FILE> files;
 
+    /* Holds all page information */
+    vector<FFS_PAGE> pages;
+    uint8_t currentPageNumber = 0;
+
+    /* Holds Backup Page information, used during Reclaim Memory Procedure */
+    FFS_PAGE backupPage;
+
+    /* yet to decide exate use of currentPage.
+    Currently it point to last Page whose PAGE_READY is set to 0x00
+
+    currentPage update in cacheAllPage()
+    */
+    FFS_PAGE currentPage;
+
+    /* old file */
+    FFS_FILE oldFile;
+
+    bool isFileExist(uint8_t fileID);
+    bool ValidateFile(uint8_t* pFile);
+    uint8_t cacheAllFiles(void);
+    FFS_FILE_HEADER createHeader(const uint8_t fileID, const uint8_t * pData, const uint16_t dataLen);
+    uint8_t crc8(const uint8_t * pData, const uint16_t dataLen);
+    FlashErrorCode writeToFlash(const uint8_t * pTargetAddr, const uint8_t * pSourceAddr, const uint16_t dataLen);
+    FfsErrorCode writeFileToFlash(FFS_FILE &file, const uint8_t* pData, const uint16_t dataLen);
+    //uint8_t writeFileToFlash(FFS_PAGE page, FFS_FILE file);
+    uint8_t checkFileIntigrity(FFS_FILE file);
+    uint8_t erasePage(FFS_PAGE page);
+    FfsErrorCode getAddressToWrite(uint16_t dataLen, uint8_t** pNewAddress);
+    uint16_t getAvailableFreeMemory(uint8_t* pPageBaseAddress) const;
+    uint8_t* getPageStartAddress(uint8_t* pAddress);
+    uint8_t* getPageStartAddress(uint8_t pageNum);
+    /* dummy function of Flash Class */
+    uint8_t getPageNumber(uint8_t *address) const;
 };
-
 
 #endif /* FFS_H */
--- FlashFileSystem/ffs_api.cpp
+++ FlashFileSystem/ffs_api.cpp
@@ -4,7 +4,8 @@
 * To change this template file, choose Tools | Templates
 * and open the template in the editor.
 */
-
+#include <iostream>
+#include <cstdio>
 #include "ffs_api.h"
 #include "ffs.h"
 
@@ -12,17 +13,29 @@ using namespace std;
 
 FFS ffs;
 
-FlashErrorCode initFlashFileSystem(uint8_t **pAddrList, const uint8_t pageCount, const uint8_t fileCount) {
-	//return ffs._initFFS(pAddrList, pageCount, fileCount);
-	return FLASH_SUCCESS;
+FfsErrorCode initFlashFileSystem(uint8_t **pAddrList, const uint8_t pageCount, const uint8_t fileCount) {
+    
+    ffs._initFFS((const uint8_t **)pAddrList, pageCount, fileCount);
+
+    //int i = 0;
+
+    //while (i < pageCount) {
+
+    //    //cout << "addr: " << hex << *(pAddrList)<<endl;
+    //    //printf("\n addr: %x", *pAddrList);
+    //    i++;
+    //    pAddrList++;
+    //}
+
+    return FFS_SUCCESS;
 }
 
-uint8_t getFile(uint8_t fileID, uint16_t ** pData, uint16_t *pDataLen) {
-	return ffs._getFileData(fileID, pData, pDataLen);
-	return 0;
+uint8_t getFile(uint8_t fileID, uint8_t ** pData, uint16_t *pDataLen) {
+    ffs._getFileData(fileID, pData, pDataLen);
+    return 0;
 }
 
 uint8_t writeFile(uint8_t fileID, uint8_t * pData, uint16_t dataLen) {
-	ffs._writeFileData(fileID, pData, dataLen);
-	return 0;
+    ffs._writeFileData(fileID, pData, dataLen);
+    return 0;
 }
